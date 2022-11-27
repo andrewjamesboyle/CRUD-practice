f02af6336fc87240d9035140720e5476e7105277
Andrew James Boyle <109110419+andrewjamesboyle@users.noreply.github.com>
2022-11-27 14:24:33 -0800

    get car by id test written

--- __tests__/cars.test.js
+++ __tests__/cars.test.js
@@ -11,85 +11,94 @@ describe('cars routes tests', () => {
   it('get returns a list of cars', async () => {
     const resp = await request(app).get('/cars');
     expect(resp.status).toEqual(200);
-    expect(resp.body).toMatchInlineSnapshot(`
-      Array [
-        Object {
-          "make": "Chevrolet",
-          "model": "Avalanche",
-          "year": 2007,
-        },
-        Object {
-          "make": "Chrysler",
-          "model": "300",
-          "year": 2010,
-        },
-        Object {
-          "make": "Ford",
-          "model": "Focus",
-          "year": 2001,
-        },
-        Object {
-          "make": "Kia",
-          "model": "Sorento",
-          "year": 2009,
-        },
-        Object {
-          "make": "Infiniti",
-          "model": "G35",
-          "year": 2008,
-        },
-        Object {
-          "make": "Saturn",
-          "model": "VUE",
-          "year": 2008,
-        },
-        Object {
-          "make": "Jeep",
-          "model": "Wrangler",
-          "year": 2001,
-        },
-        Object {
-          "make": "Mitsubishi",
-          "model": "Pajero",
-          "year": 1994,
-        },
-        Object {
-          "make": "Cadillac",
-          "model": "DeVille",
-          "year": 1998,
-        },
-        Object {
-          "make": "Chevrolet",
-          "model": "3500",
-          "year": 2000,
-        },
-        Object {
-          "make": "Nissan",
-          "model": "Xterra",
-          "year": 2004,
-        },
-        Object {
-          "make": "Mitsubishi",
-          "model": "Expo",
-          "year": 1993,
-        },
-        Object {
-          "make": "Dodge",
-          "model": "Caliber",
-          "year": 2009,
-        },
-        Object {
-          "make": "Honda",
-          "model": "Insight",
-          "year": 2011,
-        },
-        Object {
-          "make": "Porsche",
-          "model": "944",
-          "year": 1988,
-        },
-      ]
-    `);
+    expect(resp.body).toEqual([{
+      'make': 'Chevrolet',
+      'model': 'Avalanche',
+      'year': 2007,
+    },
+    {
+      'make': 'Chrysler',
+      'model': '300',
+      'year': 2010,
+    },
+    {
+      'make': 'Ford',
+      'model': 'Focus',
+      'year': 2001,
+    },
+    {
+      'make': 'Kia',
+      'model': 'Sorento',
+      'year': 2009,
+    },
+    {
+      'make': 'Infiniti',
+      'model': 'G35',
+      'year': 2008,
+    },
+    {
+      'make': 'Saturn',
+      'model': 'VUE',
+      'year': 2008,
+    },
+    {
+      'make': 'Jeep',
+      'model': 'Wrangler',
+      'year': 2001,
+    },
+    {
+      'make': 'Mitsubishi',
+      'model': 'Pajero',
+      'year': 1994,
+    },
+    {
+      'make': 'Cadillac',
+      'model': 'DeVille',
+      'year': 1998,
+    },
+    {
+      'make': 'Chevrolet',
+      'model': '3500',
+      'year': 2000,
+    },
+    {
+      'make': 'Nissan',
+      'model': 'Xterra',
+      'year': 2004,
+    },
+    {
+      'make': 'Mitsubishi',
+      'model': 'Expo',
+      'year': 1993,
+    },
+    {
+      'make': 'Dodge',
+      'model': 'Caliber',
+      'year': 2009,
+    },
+    {
+      'make': 'Honda',
+      'model': 'Insight',
+      'year': 2011,
+    },
+    {
+      'make': 'Porsche',
+      'model': '944',
+      'year': 1988,
+    },
+    ]
+    );
+  });
+
+  it('get/:id returns details of a single car', async () => {
+    const resp = await request(app).get('/cars/1');
+    const expected = {
+      id: expect.any(Number),
+      model: 'Avalanche',
+      make: 'Chevrolet',
+      year: 2007,
+    };
+    expect(resp.body).toEqual(expected);
   });
 
   afterAll(() => {
